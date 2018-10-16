d73ec42e04f20fff2f3c915d82ebaef4f621be06
Adaickalavan <adaickalavan@gmail.com>
2018-10-16 22:58:27 +0800

    Update

--- producer_python/main.py
+++ producer_python/main.py
@@ -5,19 +5,11 @@ from pykafka import KafkaClient
 def main():
 	print("Yay")
 
-
-
-
-
-
-
 	return
 
 
 
 if __name__ == "__main__":
    
-    #Close all open figures
-    plt.close("all")
     #Call main function
     main()
