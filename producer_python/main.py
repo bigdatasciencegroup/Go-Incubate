6a29352836913f52dd92da236175403efbee1c0b
Adaickalavan <adaickalavan@gmail.com>
2018-10-17 11:13:02 +0800

    Update

--- producer_python/main.py
+++ producer_python/main.py
@@ -1,23 +1,18 @@
 # import matplotlib.pyplot as plt
 # from utils import timePlot, overlapPlot
 from pykafka import KafkaClient
+from matplotlib import pyplot as plt
 
 def main():
-	print("Yay")
-
-
-
-
-
-
-
-	return
-
-
+    print("Yay")
+    # y = [1, -1, 3, -3]
+    # x = [1, 2, 3, 4]
+    # plt.plot(x,y)
+    return
 
 if __name__ == "__main__":
    
-    #Close all open figures
-    plt.close("all")
     #Call main function
     main()
+
+
