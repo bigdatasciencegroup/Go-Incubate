851701ec409a54d31f020dcb177861d1a6da23c6
Adaickalavan <adaickalavan@gmail.com>
2018-10-18 15:49:05 +0800

    Abstracted python kafka producer and consumer

---
+++ producer_python/kafkapc/__init__.py
@@ -0,0 +1,22 @@
+from kafka import KafkaConsumer, KafkaProducer
+import json
+
+class Consumer(KafkaConsumer):
+    def __init__(self, topicName, kafkaPort, consumerGroup):
+        KafkaConsumer.__init__(
+            self,
+            topicName,
+            bootstrap_servers=[kafkaPort],
+            auto_offset_reset='earliest',
+            enable_auto_commit=True,
+            group_id=consumerGroup,
+            value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+            )
+
+class Producer(KafkaProducer):
+    def __init__(self, kafkaPort):
+        KafkaProducer.__init__(
+            self,
+            bootstrap_servers=[kafkaPort],
+            value_serializer=lambda x: json.dumps(x).encode('utf-8')
+            )
---
+++ producer_python/main/main.py
@@ -0,0 +1,19 @@
+import os
+from kafkapc import Producer
+
+def main():
+
+    producer = Producer(
+        os.environ['KAFKAPORT']
+        )
+
+    for e in range(1000):
+        data = {'number': e}
+        producer.send(os.environ['TOPICNAME'], value=data)
+        print(data)
+
+    return
+
+if __name__ == "__main__":
+    #Call main function
+    main()
