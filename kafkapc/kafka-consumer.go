70909c4b84357c462cdbcafcacc3b7fb1b18db7b
Adaickalavan <adaickalavan@gmail.com>
2018-10-24 13:05:16 +0800

    update

--- kafkapc/kafka-consumer.go
+++ kafkapc/kafka-consumer.go
@@ -62,8 +62,8 @@ func ConsumeMessages(consumerParam ConsumerParam, handler messageHandler) {
 	//Consume messages from Kafka queue
 	log.Println("Waiting for messages")
 	for message := range consumer.Messages() {
-		// log.Printf("Topic: %s\t Partition: %v\t Offset: %v\n", message.Topic, message.Partition, message.Offset)
-
+		log.Printf("Topic: %s\t Partition: %v\t Offset: %v\n", message.Topic, message.Partition, message.Offset)
+		log.Println("consumerparamtopics",consumerParam.Topics[0])
 		//Only take messages from subscribed topic
 		//Potentially perform different operations on messages from different topics
 		switch message.Topic {
