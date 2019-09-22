206eaf4551525b7adaef47d199706d08264fb199
adaickalavan <adaickalavan@gmail.com>
2019-09-23 04:06:27 +0800

    Update

--- hpc/apps/prodCons.c
+++ hpc/apps/prodCons.c
@@ -3,59 +3,78 @@
 **
 **  One function generates (i.e. produces) an array of random values.  
 **  A second functions consumes that array and sums it.
-**
-**  HISTORY: Written by Tim Mattson, April 2007.
 */
+
 #include <omp.h>
-#ifdef APPLE
 #include <stdlib.h>
-#else
 #include <malloc.h>
-#endif
 #include <stdio.h>
 
-#define N        10000
+#define N 10000
 
 /* Some random number constants from numerical recipies */
-#define SEED       2531
-#define RAND_MULT  1366
-#define RAND_ADD   150889
-#define RAND_MOD   714025
+#define SEED 2531
+#define RAND_MULT 1366
+#define RAND_ADD 150889
+#define RAND_MOD 714025
 int randy = SEED;
 
 /* function to fill an array with random numbers */
 void fill_rand(int length, double *a)
 {
-   int i; 
-   for (i=0;i<length;i++) {
-     randy = (RAND_MULT * randy + RAND_ADD) % RAND_MOD;
-     *(a+i) = ((double) randy)/((double) RAND_MOD);
-   }   
+  int i;
+  for (i = 0; i < length; i++)
+  {
+    randy = (RAND_MULT * randy + RAND_ADD) % RAND_MOD;
+    *(a + i) = ((double)randy) / ((double)RAND_MOD);
+  }
 }
 
 /* function to sum the elements of an array */
 double Sum_array(int length, double *a)
 {
-   int i;  double sum = 0.0;
-   for (i=0;i<length;i++)  sum += *(a+i);  
-   return sum; 
+  int i;
+  double sum = 0.0;
+  for (i = 0; i < length; i++)
+    sum += *(a + i);
+  return sum;
 }
-  
+
+// Slide 166 - Producer Consumer
 int main()
 {
-  double *A, sum, runtime;
-  int flag = 0;
-
-  A = (double *)malloc(N*sizeof(double));
+  double sum, runtime;
+  int flag = 0, flagTemp;
+  double *A = (double *)malloc(N * sizeof(double));
 
   runtime = omp_get_wtime();
-
-  fill_rand(N, A);        // Producer: fill an array of data
-
-  sum = Sum_array(N, A);  // Consumer: sum the array
-   
+  #pragma omp parallel 
+  {
+    #pragma omp sections
+    {
+      #pragma omp section
+      {
+        fill_rand(N, A); // Producer: fill an array of data
+        #pragma omp flush
+        #pragma atomic write
+          flag = 1;
+        #pragma omp flush (flag)
+      }
+      #pragma omp section
+      {
+        while(1){
+          #pragma omp flush(flag)
+          #pragma omp atomic read
+            flagTemp = flag;
+          if(flagTemp == 1){
+            break;
+          }  
+        }
+        #pragma omp flush
+        sum = Sum_array(N, A); // Consumer: sum the array
+      }
+    }
+  }
   runtime = omp_get_wtime() - runtime;
-
-  printf(" In %f seconds, The sum is %f \n",runtime,sum);
+  printf(" In %f seconds, Sum desired %f, got %f \n", runtime, 5030.674031, sum);
 }
- 
